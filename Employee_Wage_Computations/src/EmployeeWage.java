8580faab0d4df25d7f4f3a5f85bd6d296fc9c5ce
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-06-21 18:58:26 +0530

    "[Pradip]add.Full Time Employee added

--- Employee_Wage_Computations/src/EmployeeWage.java
+++ Employee_Wage_Computations/src/EmployeeWage.java
@@ -1,35 +1,34 @@
 public class EmployeeWage {
 	void empSalary() {
-		int isFullTime = 0;
-		int isPartTime = 1;
+		int isFullTime = 1;
+		int isPartTime = 2;
 		int empRatePrHr = 20;
 		int empHr = 0;
 		int perDaySalary = 0;
-		int mothWorkingDay = 20;
-		int monthlySalary = 0;
-		for (int i = 1;i <= mothWorkingDay;i++)
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		System.out.println(empCheck);
+		switch ((int)empCheck)
 		{
-			double empCheck = Math.floor(Math.random() * 10) % 2;
-			switch ((int)empCheck)
-			{
-				case 0 :
-					empHr = 8;
-					break;
-
-				case 1 :
-					empHr = 4;
-					break;
-
-			}//switch
-			perDaySalary = empRatePrHr * empHr;
-
-			monthlySalary = perDaySalary + monthlySalary;
-
-		}
-		System.out.println("Monthly salary: "+monthlySalary);
-
+			case 1 :
+				empHr = 8;
+				System.out.println("Full time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			case 2 :
+				empHr = 4;
+				System.out.println("Part time Employee");
+				perDaySalary = empRatePrHr * empHr;
+				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			default :
+				empHr = 0;
+				perDaySalary = 0;
+				System.out.println("Employee per day salary = "+perDaySalary);
+		}//switch
 	}
 	public static void main(String[] args) {
+
 		EmployeeWage empWage = new EmployeeWage();
 		empWage.empSalary();
 
