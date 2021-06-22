7af8036d57547a6cba1263ccfe86e5ad6c84a5f4
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-06-22 15:30:55 +0530

    "[Pradip]add.Using Interface Approach Of The Company"

--- Employee_Wage_Computations/src/EmployeeWage.java
+++ Employee_Wage_Computations/src/EmployeeWage.java
@@ -1,20 +1,67 @@
-class EmplpyeeSalary {
+
+import java.util.ArrayList;
+class CompanyEmpWage {
+	public final String company;
+	public final int empRatePerHr;
+	public final int numberOfWorkingDays;
+	public final int maxHrPerMonth;
+	public int totalEmpWage;
+
+	public CompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+		this.numberOfWorkingDays = numberOfWorkingDays;
+		this.maxHrPerMonth = maxHrPerMonth;
+
+	}//constructor
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+	public String toString() {
+			return "Total employee wage for: " +company+ " is " +totalEmpWage;
+	}
+}
+
+class EmployeeWage{
         //constant
         public static final int IS_FULL_TIME = 1;
         public static final int IS_PART_TIME = 2;
 
-        public void empSalary(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth ) {
+	private int numOfCompany = 0;
+	private ArrayList<CompanyEmpWage> companyEmpWageArrayList;
+	public EmployeeWageOops() {
+		companyEmpWageArrayList = new ArrayList<>();
+
+	}
+	private void addCompanyEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth) {
+		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHr, numberOfWorkingDays, maxHrPerMonth);
+		companyEmpWageArrayList.add(companyEmpWage);
+		//numOfCompany++;
+
+
+	}
+	private void computeEmpWage() {
+		for (int i=0; i<companyEmpWageArrayList.size(); i++) {
+			CompanyEmpWage companyEmpWage = companyEmpWageArrayList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
+		}
+	}
+       private int computeEmpWage(CompanyEmpWage companyEmpWage) {
                 //variables
 
-				int empHrs = 0;
-				int totalEmpHrs = 0;
-				int totalWorkingDays = 0;
-				while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numberOfWorkingDays) {
-					totalWorkingDays++;
-					double empCheck = Math.floor(Math.random() * 10) % 3;
-					switch ((int)empCheck)
-                        {
-                                case IS_FULL_TIME :
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		//int totalEmpWage = 0;
+
+		//computation
+		while (totalEmpHrs <= companyEmpWage.maxHrPerMonth && totalWorkingDays < companyEmpWage.numberOfWorkingDays) {
+
+			totalWorkingDays++;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int)empCheck) {
+                        	case IS_FULL_TIME :
                                         empHrs = 8;
                                         break;
                                 case IS_PART_TIME :
@@ -23,21 +70,27 @@ class EmplpyeeSalary {
                                 default :
                                         empHrs = 0;
 
-                        }//switch
-						totalEmpHrs += empHrs;
-						System.out.println(totalWorkingDays+" "+totalEmpHrs);
-				}//while
+			}//switch
+			totalEmpHrs += empHrs;
+			System.out.println("totalWorkingDays: "+totalWorkingDays+"/ daly empHrs: "+empHrs+"/ totalEmpHrs: "+totalEmpHrs);
+		}//while
 
-                        int totalEmpWage = totalEmpHrs * empRatePerHr;
-                        System.out.println(company+" "+totalEmpWage);
+		return totalEmpHrs * companyEmpWage.empRatePerHr;
+
 
         }//empSalary()
-}
-public class EmployeeWage {
+		//public String toString() {
+		//	return "Total employee wage for: " +company+ "is " +totalEmpWage;
+		//}
+
 
         public static void main(String[] args) {
-                EmplpyeeSalary empWage = new EmplpyeeSalary();
-                empWage.empSalary("DMart", 20, 20, 100);
-				empWage.empSalary("icici", 50, 20, 100);
+        	EmployeeWage empWageBuilder = new EmployeeWage();
+		empWageBuilder.addCompanyEmpWage("icici", 50, 20, 100);
+		empWageBuilder.addCompanyEmpWage("DMart", 20, 20, 100);
+		empWageBuilder.addCompanyEmpWage("accenture", 100, 20, 100);
+		empWageBuilder.addCompanyEmpWage("IBM", 100, 20, 100);
+		empWageBuilder.addCompanyEmpWage("google", 100, 20, 100);
+		empWageBuilder.computeEmpWage();
         }//main()
-}//mainclass
+}//class
